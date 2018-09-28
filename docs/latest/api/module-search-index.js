f83357f6457a139cdca40f6052cbb02c104e967f
Prashant Bhat <prashant.mr@gmail.com>
2018-09-28 17:37:06 +0800

    publish pustike-eventbus-1.5.0 javadocs

---
+++ docs/latest/api/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"io.github.pustike.eventbus"}]
---
+++ docs/latest/api/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"m":"io.github.pustike.eventbus","l":"io.github.pustike.eventbus"}]
